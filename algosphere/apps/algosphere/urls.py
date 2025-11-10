e9d3913e566760f50232947349480e6bfe5aef23
Bharathmn-2005 <bharathmn006@gmail.com>
2025-11-10 10:18:05 +0530

    auth

--- algosphere/apps/algosphere/urls.py
+++ algosphere/apps/algosphere/urls.py
@@ -5,10 +5,12 @@ app_name = 'algosphere'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('auth/', views.auth_view, name='auth'),
     path('arrays', views.arrays, name='arrays'),
     path('stacks', views.stacks, name='stacks'),
     path('queues', views.queues, name='queues'),
     path('trees', views.trees, name='trees'),
     path('graphs', views.graphs, name='graphs'),
     path('comparison', views.comparison, name='comparison'),
+    path('logout/', views.logout_view, name='logout'),
 ]
